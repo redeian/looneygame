bd398e0d508640913e2e0892489d352dfd1f4532
Chatchai Wangwiwana <redeian@gmail.com>
2019-09-05 12:08:50 +0700

    Add a full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,21 +1,32 @@
 extends Control
 
-var story = "Hello my name is %s My last name is %s My pet is %s My second pet is %s"
-var answers = ["Mark", "Wang", "Cat", "Dog"]
+var story = "Hello my name is %s My last name is %s My pet is %s My second pet is %s "
+var answers = []
 var questions = []
+var question_number = 0
 
 func _ready(): 
-	print( story % answers ) 
 
-	$DisplayText.text  = "Welcome!!! \nWhat is your name?"
-	
 	questions.append("Welcome!!! \nWhat is your name?")
 	questions.append("What is your last name?")
 	questions.append("What is your pet?")
 	questions.append("What is your second pet?")
 	
+	$DisplayText.text = questions[0]
+	
 
 func _on_PlayerButton_pressed():
-	pass
+	
+	if question_number < len(questions)-1:
+		answers.append( $PlayerInput.text )
+		print( answers )
+		$DisplayText.text = questions[question_number+1]
+		question_number = question_number + 1
 
+	elif question_number == len(questions)-1:
+		answers.append( $PlayerInput.text )
+		print( answers )
+		$DisplayText.text = story % answers
+		
+	$PlayerInput.text = ""
 	
